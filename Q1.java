bed612dc2298fa211c0d1a6cff0afbdd5e26b937
sathaiah33 <88027170+sathaiah33@users.noreply.github.com>
2021-07-27 14:06:41 +0530

    Add files via upload

---
+++ Q1.java
@@ -0,0 +1,73 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package labNO7;
+ import java.io.*; 
+import java.util.*; 
+/**
+ *
+ * @author rakesh
+ */
+public class Q1 {
+  
+  
+
+    public static void main(String[] args) 
+    { 
+         
+        int n = 10,avg,t=0,max=0,min=0; 
+        Scanner in=new Scanner (System.in);
+        // Declaring the ArrayList with 
+        // initial size n 
+        ArrayList<Integer> a = new ArrayList<>(n); 
+  
+        // Appending new elements at 
+        // the end of the list 
+        for (int i = 1; i <= n; i++) 
+        {
+            int s;
+            System.out.println("Enter the elements"); 
+            s=in.nextInt();
+            a.add(s); 
+        }
+        // Printing elements 
+        System.out.println(a); 
+        for (int i = 1; i <= n; i++) 
+        {
+            
+            t= t+a.get(i);
+        }
+        avg=t/n;
+        // Printing average
+        System.out.println("average of elements:"+avg);
+   for (int i = 1; i <= n; i++) 
+        {
+            if(a.get(i)>max)
+            {
+                max=a.get(i);
+            }
+            else if(a.get(i)<min)
+            {
+                min=a.get(i);
+            }
+            else
+            {
+                continue;
+            }
+        }
+        System.out.println("maximum of elements:"+max);
+        System.out.println("minimum of elements:"+min);
+        System.out.println("even numbers of elements:");
+         for (int i = 1; i <= n; i++) 
+        {
+            if((a.get(i)%2)==0)
+            {
+           System.out.println(a.get(i));
+            }
+        }
+    } 
+}
+    
+
---
+++ Q2.java
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package labNO7;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Scanner;
+/**
+ *
+ * @author rakesh
+ */
+public class Q2{
+    
+    
+    public static void main(String[] args) {
+         Scanner in=new Scanner (System.in);
+        employee e1=new employee("raj",20000);
+        
+        ArrayList<employee> a=new ArrayList<>();
+          for (int i = 1; i <= 20; i++) 
+        {
+            int s;
+            String name;
+            System.out.println("Enter the name"); 
+            s=in.nextInt();
+            System.out.println("Enter the salary"); 
+            name=in.next();
+            a.add(new employee(name,s)); 
+        }
+        System.out.println(a);
+        Collections.sort(a,new namesort());
+        System.out.println(a);
+        
+    }
+    
+}
+class employee
+{
+    String name;
+    double salary;
+    employee(String n,double s)
+    {
+        name=n;
+        salary=s;
+    }
+    public String toString()
+    {
+        return "\nEmployee Name:"+name+"\tSalary:"+salary;
+    }
+}
+class namesort implements Comparator<employee>
+{
+    @Override
+    public int compare(employee e1,employee e2)
+    {
+        System.out.println(e1.name+"----"+e2.name);
+        return e2.name.compareTo(e1.name);
+    }
+}
+
